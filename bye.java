c53006281c24efc0a8612020c01f26a8f75e7964
Edinson <edinsoncortes1@gmail.com>
2024-02-12 12:22:16 -0500

    otro cambio en bye

---
+++ bye.java
@@ -0,0 +1,23 @@
+public class bye {
+    
+    private String bye;
+
+    public bye (){
+
+    }
+
+ 
+    public bye(String bye) {
+        this.bye = bye;
+    }
+
+
+    public String getBye() {
+        return bye;
+    }
+
+    public void setBye(String bye) {
+        this.bye = bye;
+    }
+    
+}
